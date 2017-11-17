c9159fb674041f5ee99166ce735d3007e1167667
ttran9 <toddtran9@gmail.com>
2017-11-16 18:43:47 -0800

    Implemented concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
   validates_presence_of :title, :percent_utilized
+  # include Placeholder
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= Placeholder.image_generator('250', '250')
+  end
 end
