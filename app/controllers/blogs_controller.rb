7990870223215baa26c53f80944e53262cdc53c6
ttran9 <toddtran9@gmail.com>
2017-11-20 17:35:30 -0800

    demonstrating puts debugging

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,11 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    # @blogs = Blog.all
+    @blogs = Blog.sp
+    puts "*" * 500
+    puts @blogs.inspect
+    puts "*" * 500
     @page_title = "My Portfolio Blog"
   end
 
