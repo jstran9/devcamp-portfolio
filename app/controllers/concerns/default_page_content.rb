29e3c2c3af23e61ad3bc6a1b558ee1d9396b57cd
ttran9 <toddtran9@gmail.com>
2017-11-21 18:13:49 -0800

    Implemented partials for blog, font awesome, and pagination

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -2,11 +2,15 @@ module DefaultPageContent
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_title
+    before_action :set_title, :set_github_link
   end
 
   def set_title
     @page_title = "Devcamp Portfolio | My Portfolio website"
     @seo_keywords = "Todd T portfolio"
+  end
+
+  def set_github_link
+    @todd_github_link = "https://github.com/ttran9"
   end
 end
