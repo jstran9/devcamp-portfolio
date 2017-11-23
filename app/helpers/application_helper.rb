ffb34c84d8253abc4d55f432931f7aad896a6027
ttran9 <toddtran9@gmail.com>
2017-11-22 21:19:12 -0800

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,4 +58,18 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    # creates message
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Todd T's Portfolio", sticky: false)
+  end
+
+
 end
