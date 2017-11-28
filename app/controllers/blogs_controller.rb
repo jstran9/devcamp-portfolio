c8a34b4a49262b7802786460f73bb55253b75e74
ttran9 <toddtran9@gmail.com>
2017-11-27 22:29:21 -0800

    Refactoring blog index to get sorted blog posts.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,8 +7,12 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.get_most_recent_blogs logged_in?(:admin), params
-    @page_title = "My Portfolio Blog"
+    if logged_in?(:site_admin)
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
+      @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
